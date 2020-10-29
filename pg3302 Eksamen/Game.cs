0396a78ceeaed5ad2d9e05d9b52d4a0558f61503
97krihop <97krihop@gmail.com>
2020-10-29 17:34:09 +0100

    added enum for card

--- pg3302 Eksamen/Game.cs
+++ pg3302 Eksamen/Game.cs
@@ -4,7 +4,82 @@ using System.Text;
 
 namespace pg3302_Eksamen
 {
+	enum cardHearts
+	{
+		Ace = 1,
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+	}
+	enum cardSpades
+	{
+		Ace = 1,
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+	}
+
+	enum cardDiamonds
+	{
+		Ace = 1,
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+	}
+
+	enum cardClubs
+	{
+		Ace = 1,
+		Two,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King,
+	}
+
+	enum cardSpecial
+	{
+		Bomben,
+		Gribben,
+		Karantene,
+		Jokeren
+	}
 	class Game
 	{
+		
 	}
 }
